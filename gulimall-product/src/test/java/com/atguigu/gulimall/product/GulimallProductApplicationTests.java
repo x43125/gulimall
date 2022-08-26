445de78ab9d21dcb7be3f7904970ff70391382f2
x43125 <wx43125@163.com>
2022-08-27 00:12:25 +0800

    @Description: 添加product基本代码 @Author: x43125

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,31 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+	@Autowired
+	private BrandService brandService;
+
 	@Test
 	void contextLoads() {
+//		BrandEntity brandEntity = new BrandEntity();
+//		brandEntity.setDescript("wawawa");
+//		brandEntity.setBrandId(1L);
+//		brandService.updateById(brandEntity);
+
+//		brandEntity.setName("wawei");
+//		brandService.save(brandEntity);
+//		System.out.println("保存成功");
+
+		BrandEntity entity = brandService.getById(1);
+		System.out.println(entity);
+
 	}
 
 }
